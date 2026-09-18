906f192d4ece307e6524968389572d1576da5a3b
CoolVeryVery <georgekuklevwork@gmail.com>
2026-09-18 15:06:17 +0300

    Add model and Connect to PostgresDB

--- internal/db/postgres.go
+++ internal/db/postgres.go
@@ -1,7 +1,32 @@
 package db
 
+import (
+	"database/sql"
+	"time"
+)
 
-
+//Названия таблиц
 const (
 	usersTable = "users"
 )
+//Функция подключения к базе данных
+func ConnectPostgres(url string) (*sql.DB, error) {
+	//Открываем соединение
+	db, err := sql.Open("postgres", url)
+	if err != nil {
+		return nil, err
+	}
+	//Задаем параметры
+	//Максимальное времени жизни соединения
+	db.SetConnMaxLifetime(time.Minute * 5)
+	//Максимальное количество соединений, хранящихся в пуле
+	db.SetMaxIdleConns(5)
+	//Максимальное количество активных соединений
+	db.SetMaxOpenConns(10)
+	//Если пинг к бд не прошел, то вернем err
+	if err := db.Ping(); err != nil {
+		return nil, err
+	}
+	
+	return db, nil
+}
--- internal/models/rsaUser.go
+++ internal/models/rsaUser.go
@@ -1,1 +1,13 @@
 package models
+
+//Структура User из БД
+//json - как называть поле при сериализации
+//db - как называется поле в БД для сопоставления в запросах
+//binding:"required" - то, что строка не пустая, число не 0 и т.п., есть другие теги
+type User struct {
+	UserId int 			`json:"user_id" db:"user_id"` 
+	RsaNum string 		`json:"rsa_num" binding:"required" db:"rsa_num"` 
+	RsaKey string 		`json:"rsa_key" binding:"required" db:"rsa_key"`
+	UserNameFull string `json:"user_name_full" binding:"required" db:"user_name_full"`
+	UserLogin string 	`json:"user_login" binding:"required" db:"user_login"`
+}
